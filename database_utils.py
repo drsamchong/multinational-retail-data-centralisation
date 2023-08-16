7486f900e70d85837afb8c91f4a9b3940df238ca
Sam Chong <samchong@Sams-Mac-mini.local>
2023-08-16 20:55:26 +0100

    Implements utility methods for reading DB credentials, initialising DB engine and listing DB tables.

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,6 @@
 import yaml
+import psycopg2
+from sqlalchemy import create_engine, inspect
 
 class DatabaseConnector():
 
@@ -9,9 +11,36 @@ class DatabaseConnector():
     def read_db_creds(self):
         with open(self.creds_file, mode="r") as creds:
             db_creds = yaml.safe_load(creds)
-            print(db_creds)
+#            print(db_creds)
+            return db_creds
+
+    def init_db_engine(self, db_creds):
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        USER = db_creds["RDS_USER"]
+        PASSWORD = db_creds["RDS_PASSWORD"]
+        ENDPOINT = db_creds["RDS_HOST"]
+        PORT = db_creds["RDS_PORT"]
+        DATABASE = db_creds["RDS_DATABASE"]
+        url = f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}"
+        engine = create_engine(url)
+        # TODO: read db_creds and initialise and return a sqlalchemy db engine
+        return engine
+
+    def list_db_tables(self, db_engine):
+        # SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'
+        inspector = inspect(db_engine)
+        tables = inspector.get_table_names()
+        print("Tables in DB:")
+        for table in tables:
+            print(table) 
+        
 
 
-
-# conn = DatabaseConnector()
-# conn.read_db_creds()
+if __name__ == "__main__":
+    conn = DatabaseConnector()
+    db_creds = conn.read_db_creds()
+#    print(db_creds)
+    engine = conn.init_db_engine(db_creds)
+    #engine.connect()
+    conn.list_db_tables(engine)
