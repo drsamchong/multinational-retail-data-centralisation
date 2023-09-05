394f1a6927ac013dc1b1a042886de4febc313790
Sam Chong <samchong@Sams-Mac-mini.local>
2023-09-05 16:17:49 +0100

    Adds method to DataCleaner class to convert columns to category type and to clean email addresses. Updates main user cleaning method to class new methods.

--- data_cleaning.py
+++ data_cleaning.py
@@ -16,9 +16,19 @@ class DataCleaning():
         date_cols = ["date_of_birth", "join_date"]
         for col in date_cols:
             clean_df.loc[:, col] = self.convert_date_column(clean_df, col)
+        
+        # drop any rows with unconverted, missing dates (unfathomable values in all columns)
+        clean_df.dropna(subset=date_cols, inplace=True)
 
         # correct country code to match country name
         clean_df = self.correct_country_code(clean_df)
+#        print(f"Missing values: {clean_df.isna().sum()}")
+
+        # change dtype of the country and country_code columns to category
+        clean_df = self.categorise_columns(clean_df, ["country", "country_code"])
+
+        # clean email address values
+        clean_df = self.clean_email_addresses(clean_df, "email_address")
 
         return clean_df
 
@@ -33,6 +43,8 @@ class DataCleaning():
         new_dates = pd.to_datetime(df[col_name], format=date_format, errors="coerce") \
                       .fillna(pd.to_datetime(df[col_name], format=date_format2, errors="coerce"))\
                       .fillna((pd.to_datetime(df[col_name], format=date_format3, errors="coerce")))
+        
+        print(f"Missing {col_name} dates: {new_dates.isna().sum()}")
 
         return new_dates
 
@@ -47,4 +59,19 @@ class DataCleaning():
         for country, code in country_code_map.items():
             df.loc[(df["country"] == country) & (df["country_code"] != code), "country_code"] = code
 
+        return df
+    
+    def categorise_columns(self, df, col_names):
+        """Convert dtype of columns in list to category"""
+
+        for column in col_names:
+            df[column] = df[column].astype("category")
+        
+        return df
+
+        
+    def clean_email_addresses(self, df, col_name="email_address"):
+        """Removes double at symbol @@ in email addresses"""
+
+        df[col_name] = df[col_name].str.replace("@@", "@")
         return df
