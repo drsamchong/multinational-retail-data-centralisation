2a751896c799062777b0e38a98eadae14cdb930e
Sam Chong <sam_chong@gmx.com>
2023-09-27 20:53:33 +0100

    Adds a method to the DataExtractor class to get data for all stores from the API and return a dataframe containing the store data.

--- data_extraction.py
+++ data_extraction.py
@@ -25,3 +25,41 @@ class DataExtractor():
         response = requests.get(url, headers=headers)
         n_stores = int(response.json()["number_stores"])
         return n_stores
+    
+    def retrieve_stores_data(self, url):
+
+        root_url = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/"
+        store_path = "prod/store_details/"
+        n_store_path = "prod/number_stores"
+
+        headers = {
+            "x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"
+        }       
+
+        n_stores = self.list_number_of_stores(root_url+n_store_path, headers)
+
+
+        store_df = pd.DataFrame()
+        for store in range(n_stores):
+            print(f"getting store {store}")
+            store_endpoint = f"{url}{str(store)}"
+            response = requests.get(store_endpoint, headers=headers)
+            store_data = response.json()
+            store_df = pd.concat([store_df, pd.DataFrame(store_data, index=[store_data["index"]])])
+        
+        return store_df
+    
+
+if __name__ == "__main__":
+
+    de = DataExtractor()
+    store_info_url = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/"
+    store_data = de.retrieve_stores_data(store_info_url)
+    print(store_data.info())
+    store_data.to_csv("store_data.csv")
+
+
+
+
+
+
